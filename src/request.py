58b0b8c0a4d118cf15e6df0f3844507fb5b8870d
dsradecki <radeckim00@gmail.com>
2021-01-25 16:16:13 +0100

    Initial logic of performed queries

--- src/request.py
+++ src/request.py
@@ -7,17 +7,41 @@ class Add(Request):
     def __init__(self, body: dict):
         super().__init__(body)
 
+    def perform_query(self) -> bool:
+        def generate_query():
+            #change body (dictionary) to a particurlar SQL string here
+            pass
+        #database.perform_query(SQL_query)
+
 
 class Delete(Request):
     def __init__(self, body: dict):
         super().__init__(body)
+
+    def perform_query(self) -> bool:
+        def generate_query():
+            #change body (dictionary) to a particurlar SQL string here
+            pass
+        #return database.perform_query(SQL_query)
 
 
 class List(Request):
     def __init__(self, body: dict):
         super().__init__(body)
 
+    def perform_query(self) -> bool:
+        def generate_query():
+            #change body (dictionary) to a particurlar SQL string here
+            pass
+        #return database.perform_query(SQL_query)
+
 
 class Update(Request):
     def __init__(self, body: dict):
         super().__init__(body)
+
+    def perform_query(self) -> bool:
+        def generate_query():
+            #change body (dictionary) to a particurlar SQL string here
+            pass
+        #return database.perform_query(SQL_query)
